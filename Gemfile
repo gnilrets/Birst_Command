2d88649b2a727b55e7e73e39490eebf883cbb4a8
Sterling Paramore <sterling.paramore@greatvines.com>
2014-06-02 10:14:42 -0700

    Replaced custom password handling with envcrypt

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'savon', '~> 2.0'
 gem 'httpclient'
+gem 'envcrypt', '~> 0.1'
--- test/standard/test_password.rb
+++ test/standard/test_password.rb
@@ -3,52 +3,19 @@ require "test_birst_command"
 class Test_password < Test::Unit::TestCase
 
   def setup
-    ENV['BIRST_COMMAND_IV']   = "3Ez9fL0Jlt/E1d7QlVtKdw=="
-    ENV['BIRST_COMMAND_KEY']  = "N589Xi0YzzkE+bRGwp3yaoVk/lneYsLHdFP+366hwcY="
-    ENV['BIRST_COMMAND_SALT'] = "AUkJj8QSmNW3QazpyNl7og=="
+    ENV['ENVCRYPT_KEY'] = '9Aqck/FZ0pCRkiw95VpxLw==$kxnYLOCo9qHDHHaTZM+fN73WVclDkRqO+uxSgzFzrpQ=$qoEtCm1BQWgc+WAxpotsrw=='
 
     @password = "mysecretpass"
-    @encrypted = "dP5+BfQyTAvKOM6s1ik4zg=="
+    @encrypted = "MBTkxkMT8AbQupkOwtG9uQ=="
   end
 
   def teardown
   end
 
-  def test_key_generation
-    ENV['BIRST_COMMAND_IV']   = nil
-    ENV['BIRST_COMMAND_KEY']  = nil
-    ENV['BIRST_COMMAND_SALT'] = nil
-
-    Password.generate_keys
-
-    encrypted = Password.encrypt(@password)
-    decrypted = Password.decrypt(encrypted)
-
-    assert_equal @password, decrypted, "Wrong decrypted password"
-  end
-
-
-  def test_decryption_failure
-    Password.generate_keys
-
-    encrypted = Password.encrypt(@password)
-    ENV['BIRST_COMMAND_SALT'] = SecureRandom.base64
-
-    assert_raise OpenSSL::Cipher::CipherError do
-      decrypted = Password.decrypt(encrypted)
-    end
-  end
-
-
-  def test_encrypt
-    assert_equal @encrypted, Password.encrypt(@password), "Expecting encrypted password #{@encrypted}"
-  end
-
   def test_decrypt
-    assert_equal @password, Password.decrypt(@encrypted), "Expecting decrypted password #{@password}"
+    crypt = Envcrypt::Envcrypter.new
+    assert_equal @password, crypt.decrypt(@encrypted), "Wrong decrypted password"
   end
-
-
 end
 
 
