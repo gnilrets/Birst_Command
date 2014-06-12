6a1d5a61d7f7944aeaf6d354ec1d66d70b6fc718
Sterling Paramore <sterling.paramore@greatvines.com>
2014-06-12 16:40:06 -0700

    Run specs w or w/o live connection

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -1,23 +1,11 @@
 require 'savon/mock/spec_helper'
 
-describe "running a session" do
+describe "Sessions" do
   include Savon::SpecHelper
 
-  before do
-    Settings.session.soap_log_level = :debug
-    savon.mock!
-  end
+  before { Settings.session.soap_log_level = :debug }
 
-  after { savon.unmock! }
-
-  describe "logging in and out" do
-    before do
-      crypt = Envcrypt::Envcrypter.new
-
-      message = { :username => Settings.session.username, :password => crypt.decrypt(Settings.session.password) }
-      savon.expects(:login).with(message: message).returns(BCSpecFixtures.login)
-      savon.expects(:logout).with(message: { :token => BCSpecFixtures.login_token }).returns(BCSpecFixtures.logout)
-    end
+  shared_examples_for "Log in and out" do
 
     context "by calling the object methods directly" do
       specify "without options" do
@@ -33,7 +21,7 @@ describe "running a session" do
       end
     end
 
-    context "in a session block", :focus => true do
+    context "in a session block" do
       specify "without options" do
         Session.new do |bc|
         end
@@ -44,6 +32,26 @@ describe "running a session" do
         end
       end
     end
+  end
 
+
+
+  context "with mock objects" do
+    before do
+      savon.mock!
+      crypt = Envcrypt::Envcrypter.new
+
+      message = { :username => Settings.session.username, :password => crypt.decrypt(Settings.session.password) }
+      savon.expects(:login).with(message: message).returns(BCSpecFixtures.login)
+      savon.expects(:logout).with(message: { :token => BCSpecFixtures.login_token }).returns(BCSpecFixtures.logout)
+    end
+
+    after { savon.unmock! }
+
+    it_behaves_like "Log in and out"
+  end
+
+  context "with live connection to BWS", :live => true do
+    it_behaves_like "Log in and out"
   end
 end
